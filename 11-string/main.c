33ff068632505f8c7c8140a7b7b9bb393df79b91
geri35 <geri@upi.edu>
2024-12-28 20:21:14 +0700

    edit:string main

--- 11-string/main.c
+++ 11-string/main.c
@@ -11,5 +11,16 @@ int main(void){
     printf("%s\n",text);
     int length = strlen(mystring); //strlen ada fungsi untuk menghitung panjang string bawaan
     printf("length : %d\n", length);
+
+    // penggunaan strcpy
+    char str1[] = "tampan-tampan masuk";
+    char str2[50];
+
+    if (strncpy(str2, str1, sizeof(str2)) == str2) {
+        printf("%s\n", str2);
+        printf("nasiuduk\n");
+    } else {
+        printf("Error: tidak dapat menyalin string\n");
+    }
     return 0;
 }
