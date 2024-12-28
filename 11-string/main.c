914ef42321efac7c7e06d3e382de411454841b9c
geri35 <geri@upi.edu>
2024-12-28 20:02:48 +0700

    edit:string

--- 11-string/main.c
+++ 11-string/main.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+#include<string.h> //library untuk memanggil strlen
 
 int main(void){
     
     char name[20] = "Tampan sipit";
+    char text[]="aku dari cina";
+    char mystring[] ="some string";
 
     printf("%s\n", name);
+    printf("%s\n",text);
+    int length = strlen(mystring); //strlen ada fungsi untuk menghitung panjang string bawaan
+    printf("length : %d\n", length);
     return 0;
 }
